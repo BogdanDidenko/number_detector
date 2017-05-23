a17ec2f1538634fdee5e31f7bd3effc1012c8e5d
Bogdan <b.didenko@gmail.com>
2017-05-24 00:35:02 +0300

    add env port

--- index.js
+++ index.js
@@ -57,6 +57,6 @@ app.post('/data', function (req, res) {
   var result = proccessData( req.body.data, res);
 });
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log('Example app listening on port 3000!');
 });
